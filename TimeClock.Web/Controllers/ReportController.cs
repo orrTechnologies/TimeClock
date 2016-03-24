b6a2b70617fd2c93684c595c3bc8ab0898237e4e
Dylan <dylan@cruisercorps.com>
2016-03-24 16:58:46 -0500

    Initial commit of the report controller, work in progress.

---
+++ TimeClock.Web/Controllers/ReportController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Proxies;
+using System.Web;
+using System.Web.Mvc;
+using TimeClock.Data.Models;
+using TimeClock.Web.Models;
+using TimeClock.Web.Services;
+
+namespace TimeClock.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public ReportController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        //
+        // GET: /Report/
+        public ActionResult Index()
+        {
+            ReportIndex view = new ReportIndex();
+            List<Employee> employees = _employeeService.GetEmployeeList();
+
+            MultiSelectList list = new MultiSelectList(employees);
+
+            return View(new ReportIndex()
+            {
+                Employees = list
+            });
+
+        }
+        [HttpPost]
+        public ActionResult Index(ReportRequest reportRequest)
+        {
+            return null;
+        }
+
+        public ActionResult TimeReport(ReportRequest reportRequest)
+        {
+            return null;
+        }
+	}
+}
---
+++ TimeClock.Web/Models/ReportViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeClock.Web.Models
+{
+    public class ReportRequest
+    {
+        public MultiSelectList Employees { get; set; }
+        public IEnumerable<int> EmployeeIds { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+
+    public class ReportIndex
+    {
+        public MultiSelectList Employees { get; set; }
+        public IEnumerable<SelectListItem> EmployeeIds { get; set; }
+
+    }
+}
