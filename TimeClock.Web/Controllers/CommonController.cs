8a394bf187c3592ceb58996634d79d365b3ab231
OrrTech <orrtechnologies@gmail.com>
2016-04-13 21:14:52 -0500

    New common controller, move menu to its on view.

---
+++ TimeClock.Web/Controllers/CommonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TimeClock.Web.Controllers
+{
+    public class CommonController : Controller
+    {
+        //
+        // GET: /Common/
+        [ChildActionOnly]
+        public ActionResult SideBarMenu()
+        {
+            var model = new SideBarMenuViewModel();
+            model.IsAdmin = Roles.IsUserInRole("Admin");
+
+
+            return PartialView(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class SideBarMenuViewModel
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
