063e86c28cbe2e8546705bf95f795d211e3990f3
Dylan <dylan@cruisercorps.com>
2016-06-30 09:59:25 -0500

    Now using angularMoment for dateTimeFormatting.

--- Timeclock.Api/app/app.js
+++ Timeclock.Api/app/app.js
@@ -1,4 +1,4 @@
-﻿var timeClock = angular.module('timeClock', ['ngRoute', 'ngResource', 'ui.bootstrap', "ngTable",
+﻿var timeClock = angular.module('timeClock', ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngTable', 'angularMoment',
                                 'timeClock.statusMessage', 'timeClock.employee', 'timeClock.admin'])
         .config(function ($routeProvider) {
         $routeProvider.otherwise({ templateUrl: 'app/components/employees/employees.html', controller: 'EmployeeListController' });
