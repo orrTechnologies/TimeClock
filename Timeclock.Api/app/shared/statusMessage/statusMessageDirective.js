99fde4a66a3ce43507edbec183134f51f011aaa9
Dylan <dylan@cruisercorps.com>
2016-10-06 14:36:36 -0500

    Re add status message directive

---
+++ Timeclock.Api/app/shared/statusMessage/statusMessageDirective.js
@@ -0,0 +1,13 @@
+timeClock.directive('statusMessage', function() {
+ -    return {
+ -        restrict: 'E',
+ -        templateUrl: "/templates/statusMessage/statusMessage.html",
+ -        replace: true,
+ -        scope: {
+ -            statusMessage: "=message",
+ -            remove: "&"
+ -        },
+ -        link: function() {
+ -        }
+ -    }
+ -});
